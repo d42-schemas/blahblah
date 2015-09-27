996bdf8130cca5d524834fdd391a4c3791d27fca
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-09-27 20:24:40 +0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+  name='blahblah',
+  version='0.1',
+  url='https://github.com/nikitanovosibirsk/blahblah',
+  author='Nikita Tsvetkov',
+  author_email='nikitanovosibirsk@yandex.com',
+  license='MIT',
+  packages=['blahblah']
+)
