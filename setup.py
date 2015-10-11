761f8e2a9114f4f067de25bc7ba6f177a23fe1e0
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-10-11 23:09:06 +0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
   name='blahblah',
-  version='0.2.0',
+  packages=find_packages(),
+  version='0.5.2',
+  description='Fake data generator for district42 schema',
   url='https://github.com/nikitanovosibirsk/blahblah',
   author='Nikita Tsvetkov',
   author_email='nikitanovosibirsk@yandex.com',
   license='MIT',
-  packages=['blahblah']
+  install_requires=[
+    'district42==0.5.2'
+  ]
 )
