478833b7c9d0e40d2c37765eaa634731ddeab0e6
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-10-19 17:46:04 +0600

    bump version: 0.5.2 -> 0.5.4

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
   name='blahblah',
-  packages=find_packages(),
-  version='0.5.3',
+  version='0.5.4',
   description='Fake data generator for district42 schema',
   url='https://github.com/nikitanovosibirsk/blahblah',
   author='Nikita Tsvetkov',
   author_email='nikitanovosibirsk@yandex.com',
   license='MIT',
+  packages=find_packages(),
   install_requires=[
-    'district42==0.5.2'
+    'district42==0.5.4',
+    'exrex==0.9.3'
   ]
 )
