b735c77b7d3be2744ace25b01008b9fec88296a7
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2017-11-27 18:43:10 +0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
   name='blahblah',
-  version='0.5.8',
+  version='0.6.1',
   description='Fake data generator for district42 schema',
   url='https://github.com/nikitanovosibirsk/blahblah',
   author='Nikita Tsvetkov',
