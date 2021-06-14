f01a4140ebdb06a9d2a8fb21c0aa10436b7f0d67
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-06-14 17:23:15 +0700

    bump version (0.6.9)

--- README.md
+++ README.md
@@ -2,28 +2,9 @@
 
 Fake data generator for [district42](https://github.com/nikitanovosibirsk/district42) schema.
 
-### Usage
 
-```python
-from district42 import json_schema as schema
-from blahblah import fake
-
-fake(schema.string.numeric)
-# '5694183762298043662'
-
-fake(schema.array_of(schema.integer).length(1))
-# [776641789]
-```
-
-### Installation
+## Installation
 
 ```sh
-$ pip3 install blahblah
-```
-
-### Tests
-
-```sh
-$ python3 setup.py install
-$ python3 -m unittest tests
+pip3 install blahblah==0.6.9
 ```
--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@ from setuptools import find_packages, setup
 
 setup(
   name='blahblah',
-  version='0.6.3',
+  version='0.6.9',
   description='Fake data generator for district42 schema',
   url='https://github.com/nikitanovosibirsk/blahblah',
   author='Nikita Tsvetkov',
   author_email='nikitanovosibirsk@yandex.com',
+  python_requires='>=3.6',
   license='MIT',
   packages=find_packages(),
   install_requires=[
-    'district42==0.6.3',
-    'exrex==0.10.5'
+    'district42<1.0',
+    'exrex==0.10.5',
   ],
-  dependency_links=[
-    'https://github.com/nikitanovosibirsk/district42/tarball/e81ef1fc033e47a94b9101b05981b73a722aa1ed#egg=district42-0.6.3'
-  ]
 )
