882abc04056fe75f7ab79bb99ac73d77e30f9a1e
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-06-27 19:57:18 +0700

    add override

--- blahblah/__init__.py
+++ blahblah/__init__.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from district42 import GenericSchema
+from district42.types import Schema
 
 from ._generator import Generator
 from ._random import Random
@@ -14,3 +15,6 @@ _generator = Generator(Random())
 
 def fake(schema: GenericSchema, **kwargs: Any) -> Any:
     return schema.__accept__(_generator, **kwargs)
+
+
+Schema.__override__(Schema.__invert__.__name__, fake)
