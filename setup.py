58ed2b72631e700cbc5f468e2f1cfb0e692a7784
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-07-04 18:58:05 +0700

    bump version → 1.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="blahblah",
-    version="1.0.0",
+    version="1.1.0",
     description="Fake data generator for district42 schema",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
