0e7fa9cfdc8a4895f4f0845886899a0bd68ad8e2
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-07-11 16:50:57 +0700

    add str contains

--- blahblah/_generator.py
+++ blahblah/_generator.py
@@ -1,4 +1,3 @@
-import string
 from typing import Any, Dict, List
 
 from district42 import SchemaVisitor
@@ -23,6 +22,7 @@ from ._consts import (
     INT_MIN,
     LIST_LEN_MAX,
     LIST_LEN_MIN,
+    STR_ALPHABET,
     STR_LEN_MAX,
     STR_LEN_MIN,
 )
@@ -68,12 +68,21 @@ class Generator(SchemaVisitor[Any]):
         else:
             min_length = schema.props.min_len if (schema.props.min_len is not Nil) else STR_LEN_MIN
             max_length = schema.props.max_len if (schema.props.max_len is not Nil) else STR_LEN_MAX
+            if schema.props.substr is not Nil:
+                min_length = max(min_length, len(schema.props.substr))
+                max_length = max(max_length, len(schema.props.substr))
             length = self._random.random_int(min_length, max_length)
 
         if schema.props.alphabet is not Nil:
             alphabet = schema.props.alphabet
         else:
-            alphabet = string.printable
+            alphabet = STR_ALPHABET
+
+        if schema.props.substr is not Nil:
+            substr = schema.props.substr
+            generated = self._random.random_str(length - len(substr), alphabet)
+            offset = self._random.random_int(0, len(generated))
+            return generated[0:offset] + substr + generated[offset:]
 
         return self._random.random_str(length, alphabet)
 
--- tests/str/test_str_generation.py
+++ tests/str/test_str_generation.py
@@ -1,10 +1,11 @@
 import string
-from unittest.mock import call
+from unittest.mock import Mock, call
 
+import pytest
 from baby_steps import given, then, when
 from district42 import schema
 
-from blahblah._consts import STR_LEN_MAX, STR_LEN_MIN
+from blahblah._consts import STR_ALPHABET, STR_LEN_MAX, STR_LEN_MIN
 
 from .._fixtures import *  # noqa: F401, F403
 
@@ -24,7 +25,7 @@ def test_str_generation(*, generate, random_):
         assert STR_LEN_MIN <= len(res) <= STR_LEN_MAX
         assert random_.mock_calls == [
             call.random_int(STR_LEN_MIN, STR_LEN_MAX),
-            call.random_str(str_len, string.printable)
+            call.random_str(str_len, STR_ALPHABET)
         ]
 
 
@@ -52,7 +53,7 @@ def test_str_len_generation(*, generate, random_):
     with then:
         assert len(res) == str_len
         assert random_.mock_calls == [
-            call.random_str(str_len, string.printable)
+            call.random_str(str_len, STR_ALPHABET)
         ]
 
 
@@ -71,7 +72,7 @@ def test_str_min_len_generation(*, generate, random_):
         assert str_min_len <= len(res) <= STR_LEN_MAX
         assert random_.mock_calls == [
             call.random_int(str_min_len, STR_LEN_MAX),
-            call.random_str(str_len, string.printable)
+            call.random_str(str_len, STR_ALPHABET)
         ]
 
 
@@ -89,7 +90,7 @@ def test_str_len_max_generation(*, generate, random_):
         assert STR_LEN_MIN <= len(res) <= str_max_len
         assert random_.mock_calls == [
             call.random_int(STR_LEN_MIN, str_max_len),
-            call.random_str(str_len, string.printable)
+            call.random_str(str_len, STR_ALPHABET)
         ]
 
 
@@ -108,7 +109,7 @@ def test_str_len_min_max_generation(*, generate, random_):
         assert str_min_len <= len(res) <= str_max_len
         assert random_.mock_calls == [
             call.random_int(str_min_len, str_max_len),
-            call.random_str(str_len, string.printable)
+            call.random_str(str_len, STR_ALPHABET)
         ]
 
 
@@ -164,4 +165,82 @@ def test_str_alphabet_min_max_len_generation(*, generate, random_):
         assert random_.mock_calls == [
             call.random_int(str_min_len, str_max_len),
             call.random_str(str_len, str_alphabet)
+        ]
+
+
+@pytest.mark.parametrize("substr", ["banana", " ", ""])
+def test_str_contains_generation(substr: str, *, generate, random_):
+    with given:
+        sch = schema.str.contains(substr)
+
+        str_len = 15
+        random_.random_int.return_value = str_len
+
+    with when:
+        res = generate(sch)
+
+    with then:
+        assert substr in res
+        assert random_.mock_calls == [
+            call.random_int(len(substr), STR_LEN_MAX),
+            call.random_str(str_len - len(substr), STR_ALPHABET),
+            call.random_int(0, str_len - len(substr)),
+        ]
+
+
+@pytest.mark.parametrize("length", [5, 6, 7])
+def test_str_contains_with_len_generation(length: int, *, generate):
+    with given:
+        substr = "banana"
+        sch = schema.str.contains(substr).len(length)
+
+    with when:
+        res = generate(sch)
+
+    with then:
+        assert substr in res
+        assert len(res) == max(length, len(substr))
+
+
+@pytest.mark.parametrize("min_length", [5, 6, 7])
+def test_str_contains_with_min_len_generation(min_length: int, *, generate, random_):
+    with given:
+        substr = "banana"
+        substr_len = max(len(substr), min_length)
+        sch = schema.str.contains(substr).len(min_length, ...)
+
+        str_len = 15
+        random_.random_int = Mock(side_effect=(str_len, 0))
+
+    with when:
+        res = generate(sch)
+
+    with then:
+        assert res.startswith(substr)
+        assert random_.mock_calls == [
+            call.random_int(substr_len, STR_LEN_MAX),
+            call.random_str(str_len - len(substr), STR_ALPHABET),
+            call.random_int(0, str_len - len(substr)),
+        ]
+
+
+@pytest.mark.parametrize("max_length", [5, 6, 7])
+def test_str_contains_with_max_len_generation(max_length: int, *, generate, random_):
+    with given:
+        substr = "banana"
+        substr_len = max(len(substr), max_length)
+        sch = schema.str.contains(substr).len(..., max_length)
+
+        str_len = 15
+        random_.random_int = Mock(side_effect=(str_len, str_len - len(substr)))
+
+    with when:
+        res = generate(sch)
+
+    with then:
+        assert res.endswith(substr)
+        assert random_.mock_calls == [
+            call.random_int(len(substr), substr_len),
+            call.random_str(str_len - len(substr), STR_ALPHABET),
+            call.random_int(0, str_len - len(substr)),
         ]
