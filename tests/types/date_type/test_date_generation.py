69ddf24100c3029bd6e826cca9a1c204ba67fe06
YevgeniyBelozerov <yevgeniy.belozerov@gmail.com>
2024-07-16 16:04:31 +0700

    fix tests

--- tests/types/date_type/test_date_generation.py
+++ tests/types/date_type/test_date_generation.py
@@ -8,7 +8,7 @@ from ..._fixtures import *  # noqa: F401, F403
 
 def test_date_generation(*, generate, random_):
     with given:
-        sch = schema.date_type
+        sch = schema.date
 
     with when:
         res = generate(sch)
@@ -20,12 +20,12 @@ def test_date_generation(*, generate, random_):
 
 def test_date_value_generation(*, generate, random_):
     with given:
-        dt = date.today()
-        sch = schema.date_type(dt)
+        d = date.today()
+        sch = schema.date(d)
 
     with when:
         res = generate(sch)
 
     with then:
-        assert res == dt
+        assert res == d
         assert random_.mock_calls == []
